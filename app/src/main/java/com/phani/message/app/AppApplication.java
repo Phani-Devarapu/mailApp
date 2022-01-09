40b8fe9405878b3b465edaf52b65850b49d40a22
Phani Devarapu <phanidevarapu321@gmail.com>
2022-01-09 03:26:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/phani/message/app/AppApplication.java
@@ -0,0 +1,25 @@
+package com.phani.message.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal);
+        return principal.getAttribute("login");
+    }
+
+}
---
+++ app/src/test/java/com/phani/message/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phani.message.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
